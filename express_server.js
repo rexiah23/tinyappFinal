9d7082481fde2e123eca20b688b076a720b6ee72
Lewis Lee <lewisforbusiness@gmail.com>
2021-08-30 14:17:24 -0400

    Added urls_new.ejs and added the generateRandomString function

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -9,6 +11,9 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString () {
+  return Math.random().toString(36).substr(0, 6);
+}
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -22,6 +27,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
   res.render("urls_show", templateVars);
@@ -30,6 +39,11 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.post("/urls", (req,res) => {
+  console.log(req.body); 
+  res.send("Ok")
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
