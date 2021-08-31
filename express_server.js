ee6656e85564463861eb5530c4f2a7fdf66e5708
Lewis Lee <lewisforbusiness@gmail.com>
2021-08-31 15:40:59 -0400

    Finazlied cookie functionality to app

--- express_server.js
+++ express_server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -12,7 +14,7 @@ const urlDatabase = {
 };
 
 function generateRandomString () {
-  return Math.random().toString(36).substr(0, 6);
+  return Math.random().toString(36).substr(2, 8);
 }
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -23,7 +25,10 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase }
+  const templateVars = { 
+    username: req.cookies.username,
+    urls: urlDatabase }
+    console.log(templateVars.username)
   res.render("urls_index", templateVars);
 })
 
@@ -32,7 +37,10 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
+  const templateVars = { 
+    username: req.cookies['username'],
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL] }
   res.render("urls_show", templateVars);
 })
 
@@ -50,12 +58,28 @@ app.post("/urls", (req,res) => {
   const shortendURL = generateRandomString(); 
   urlDatabase[shortendURL] = req.body.longURL; 
   console.log(urlDatabase);
-  res.redirect(`http://localhost:8080/urls/:${shortendURL}`)
+  res.redirect(`/urls/${shortendURL}`);
+})
+
+app.post("/urls/:id", (req, res) => {
+  const newLongURL = req.body.newLongURL;
+  urlDatabase[req.params.id] = newLongURL; 
+  res.redirect('/urls');
 })
 
 app.post("/urls/9sm5xK/delete", (req, res) => {
   delete urlDatabase["9sm5xK"]; 
   res.redirect(`http://localhost:8080/urls`);
+})
+
+app.post("/login", (req,res) => {
+  res.cookie('username', req.body.username); 
+  res.redirect('/urls');
+})
+
+app.post("/logout", (req,res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
 })
 
 app.listen(PORT, () => {
